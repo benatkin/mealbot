bb0b77f7b6fba92c217af7522702efa3517f92d4
Ben Atkin <ben@benatkin.com>
2012-10-27 23:53:50 +0000

    res.body not req.body

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ function reply(message, recipients, html, callback) {
 app.post('/email', function(req, res, next) {
   log(req.body, function() {});
 
-  var message = res.body
+  var message = req.body
     , recipients = getRecipients(message)
     , params = getParams(message.text);
 
