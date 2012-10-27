f50a3cade6792a9111ef9c0a3a8d2e471999b291
Ben Atkin <ben@benatkin.com>
2012-10-27 17:15:14 +0000

    extract server; add Procfile for Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var app = require('./')
+  , http = require('http')
+  , server = http.createServer(app);
+
+module.exports = server;
+
+if (! module.parent) {
+  app.set('port', process.env.PORT || 3000);
+  server.listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+  });
+}
