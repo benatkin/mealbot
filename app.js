120f380b13530383908a678c9fff14e5e726c69e
Andy Ennamorato <virtualandy@gmail.com>
2012-10-27 17:41:09 -0600

    trying to modify favicon

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: 8400000 }));  
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
