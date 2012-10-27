98ab6b7191c6794508d5f6fef9ca22af1860f8aa
Ben Atkin <ben@benatkin.com>
2012-10-27 23:45:36 +0000

    complete regex

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ function getRecipients(message) {
 }
 
 function getParams(text) {
-  var match = /^(.*) in (.*)/.exec(text);
+  var match = /^(.*) in (.*)$/.exec(text);
   if (match) {
     return {
       location: 'Denver',
